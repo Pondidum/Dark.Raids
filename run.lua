b500922746d1c7e856c898f87c01c68e3a642265
andy <andy@andydote.co.uk>
2014-02-03 17:59:26 +0000

    markers: don't recreate the ui on each player_entering_world

--- run.lua
+++ run.lua
@@ -4,10 +4,11 @@ local core = Dark.core
 local events = core.events.new()
 
 local run = function()
-	ns.markers.new()
 	ns.threatUi.new()
 	ns.interrupt.new()
 	ns.cooldowns.new()
 end
+
+ns.markers.new()
 
 events.register("PLAYER_ENTERING_WORLD", run)
