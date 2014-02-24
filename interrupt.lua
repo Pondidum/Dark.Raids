6ffdd527db29e216d26c7f93d2c3ac093b9457b2
andy <andy@andydote.co.uk>
2014-02-24 21:29:45 +0000

    interrupt: move options ui to separate file

--- interrupt.lua
+++ interrupt.lua
@@ -4,7 +4,6 @@ local config = ns.config.interrupt
 local core = Dark.core
 local eventStore = core.events.new()
 local slash = core.slash
-local options = core.options
 
 local interrupt = {
 
@@ -105,128 +104,6 @@ local interrupt = {
 		if config.enabled then
 			this.enable()
 		end
-
-		local inputOffset = 5
-		local groupSpacing = 20
-
-		options.register("Interrupt", {
-			description = "An Interrupt announcer.",
-
-			children = {
-				{
-					type = "label",
-					name = "$parentEnabledLabel",
-					points = {
-						{ "TOPLEFT" , "$parent", "TOPLEFT", 10, -10 },
-					},
-					text = "Enabled",
-				},
-				{
-					type = "checkbox",
-					template = "OptionsCheckButtonTemplate",
-					name = "$parentEnabled",
-					points = {
-						{ "TOP", "$parentEnabledLabel", "BOTTOM", 0, -5 },
-						{ "LEFT", "$parentEnabledLabel", "LEFT", inputOffset, 0 },
-					},
-					actions = {
-						save = function(self)
-							if self:GetChecked() then
-								this.enable()
-							else
-								this.disable()
-							end
-						end,
-						load = function(self)
-							self:SetChecked(this.isEnabled())
-						end,
-					}
-				},
-				{
-					type = "label",
-					name = "$parentChannelLabel",
-					points = {
-						{ "TOP", "$parentEnabled", "BOTTOM", 0, -groupSpacing},
-						{ "LEFT", "$parentEnabledLabel", "LEFT", 0, 0 },
-					},
-					text = "Announce to Channel:"
-				},
-				{
-					type = "dropdown",
-					name = "$parentChannel",
-					points = {
-						{ "TOP", "$parentChannelLabel", "BOTTOM", 0, -5 },
-						{ "LEFT", "$parentEnabledLabel", "LEFT", inputOffset-20, 0 },
-					},
-					items = {
-						PARTY = "Party",
-						SAY = "Say",
-						RAID = "Raid",
-					},
-					actions = {
-						save = function(self)
-							this.setChannel(UIDropDownMenu_GetSelectedValue(self))
-						end,
-						load = function(self)
-							UIDropDownMenu_SetSelectedValue(self, this.getChannel())
-							UIDropDownMenu_SetText(self, this.getChannel())
-						end,
-					}
-				},
-				{
-					type = "label",
-					name = "$parentSuffixLabel",
-					points = {
-						{ "TOP", "$parentChannel", "BOTTOM", 0, -groupSpacing},
-						{ "LEFT", "$parentEnabledLabel", "LEFT", 0, 0 },
-					},
-					text = "Suffix messages with:"
-				},
-				{
-					type = "textbox",
-					name = "$parentSuffix",
-					points = {
-						{ "TOP", "$parentSuffixLabel", "BOTTOM", 0, -5},
-						{ "LEFT", "$parentEnabledLabel", "LEFT", inputOffset, 0 },
-					},
-					size = { 250, 20 },
-					actions = {
-						save = function(self)
-							this.setSuffix(self:GetText())
-						end,
-						load = function(self)
-							self:SetText(this.getSuffix())
-						end,
-					}
-				},
-				{
-					type = "label",
-					name = "$parentWhisperLabel",
-					points = {
-						{ "TOP", "$parentSuffix", "BOTTOM", 0, -groupSpacing},
-						{ "LEFT", "$parentEnabledLabel", "LEFT", 0, 0 },
-					},
-					text = "Whisper a player on successful interrupt:"
-				},
-				{
-					type = "textbox",
-					name = "$parentWhisper",
-					points = {
-						{ "TOP", "$parentWhisperLabel", "BOTTOM", 0, -5},
-						{ "LEFT", "$parentEnabledLabel", "LEFT", inputOffset, 0 },
-					},
-					size = { 150, 20 },
-					actions = {
-						save = function(self)
-							this.setNotify(self:GetText())
-						end,
-						load = function(self)
-							self:SetText(this.getNotify())
-						end,
-					}
-				},
-			},
-		})
 
 		return this
 
