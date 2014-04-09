3050ad4fe817804d24e9243c9f85859e12dbca8f
andy <andy@andydote.co.uk>
2014-04-09 22:58:45 +0100

    selfBuffs: display a warning when you forget a critical self buff

--- config.lua
+++ config.lua
@@ -46,6 +46,43 @@ local config = {
 		},
 	},
 
+	selfBuffs = {
+		buffs = {
+			DEATHKNIGHT = {
+				57330, -- horn of Winter
+				31634, -- strength of earth totem
+				6673, -- battle Shout
+				93435, -- roar of courage (hunter pet)
+			},
+			HUNTER = {
+				13165, 	-- hawk
+				5118, 	-- cheetah
+				13159, 	-- pack
+				20043, 	-- wild
+				82661, 	-- fox
+				109260,	-- iron hawk
+			},
+			MAGE = {
+				7302, -- frost armor
+				6117, -- mage armor
+				30482, -- molten armor
+			},
+			PRIEST = {
+				588, 	-- inner fire
+				73413, 	-- inner will
+			},
+			SHAMAN = {
+				52127, -- water shield
+				324, -- lightning shield
+				974, -- earth shield
+			},
+			WARRIOR = {
+				469, -- commanding Shout
+				6673, -- battle Shout
+			},
+		},
+	},
+
 }
 
 ns.config = config
---
+++ features/selfBuffs.lua
@@ -0,0 +1,108 @@
+local addon, ns = ...
+
+local events = ns.lib.events.new()
+local style = ns.lib.style
+local sounds = ns.lib.sounds
+
+local config = ns.config.selfBuffs
+
+local buffChecker = {
+
+	new = function()
+
+		local frame = CreateFrame("Frame", "DarkSelfBuff", UIParent)
+		frame:SetSize(40, 40)
+		frame:SetPoint("CENTER", UIParent, "CENTER", 0, 200)
+
+		style.addShadow(frame)
+
+		local icon = frame:CreateTexture(nil, "OVERLAY")
+		icon:SetAllPoints(frame)
+		icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+
+		frame:Hide()
+
+		local buffs = {}
+		local sound = false
+
+		local checkBuffs = function()
+
+			if UnitAffectingCombat("player") and not UnitInVehicle("player") then
+
+				for spellID, name in pairs(buffs) do
+
+					if name and UnitBuff("player", name) then
+						frame:Hide()
+						sound = true
+						return
+					end
+
+				end
+
+				frame:Show()
+
+				if sound then
+					PlaySoundFile(sounds.warning)
+					sound = false
+				end
+
+			else
+
+				frame:Hide()
+				sound = true
+			end
+
+		end
+
+		local spellsChanged = function()
+
+			local localClass, class = UnitClass("player")
+			buffs = {}
+
+			for i, spellID in ipairs(config.buffs[class] or {}) do
+				buffs[spellID] = GetSpellInfo(spellID)
+			end
+
+			for spellID, buffName in pairs(buffs) do
+
+				local usable, noMana = IsUsableSpell(buffName)
+
+				if usable or noMana then
+					icon:SetTexture(GetSpellTexture(buffName))
+					break
+				end
+
+			end
+
+			checkBuffs()
+
+		end
+
+		spellsChanged()
+
+		events.register("PLAYER_LOGIN", spellsChanged)
+		events.register("LEARNED_SPELL_IN_TAB", spellsChanged)
+
+		events.register("UNIT_AURA", checkBuffs)
+		events.register("PLAYER_REGEN_ENABLED", checkBuffs)
+		events.register("PLAYER_REGEN_DISABLED", checkBuffs)
+		events.register("UNIT_ENTERING_VEHICLE", checkBuffs)
+		events.register("UNIT_ENTERED_VEHICLE", checkBuffs)
+		events.register("UNIT_EXITING_VEHICLE", checkBuffs)
+		events.register("UNIT_EXITED_VEHICLE", checkBuffs)
+
+	end,
+}
+
+local activateBuffs = function()
+
+	events.register("PLAYER_LOGIN", function()
+
+ 		buffChecker.new()
+		events.unregister("PLAYER_LOGIN")
+
+	end)
+
+end
+
+ns.features.selfBuffs = activateBuffs
