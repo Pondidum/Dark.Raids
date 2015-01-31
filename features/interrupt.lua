397059ecd01ddf85f59a8d2521fe5354bff071f1
andy <andy@andydote.co.uk>
2015-01-31 22:40:03 +0000

    interrupt: convert to new events mixin

--- features/interrupt.lua
+++ features/interrupt.lua
@@ -1,17 +1,23 @@
 local addon, ns = ...
-local events = ns.lib.events.new()
 
-local run = function()
+local class = ns.lib.class
+local events = ns.lib.events
 
-	local interrupt = ns.features.interrupt
+local run = class:extend({
 
-	events.register("PLAYER_LOGIN", function()
+	ctor = function(self)
+		self:include(events)
+		self:register("PLAYER_LOGIN")
+	end,
+
+	PLAYER_LOGIN = function(self)
 
 		local controller = interrupt.controller.new()
 		local options = interrupt.options.register(controller)
 
-	end)
-	
-end
+	end,
+})
 
-ns.features.runInterrupt = run
+ns.features.runInterrupt = function()
+	run:new()
+end
